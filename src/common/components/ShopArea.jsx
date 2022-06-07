f00088e0ff4323a3aed46b60495acea1033037c3
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-07 15:22:36 +0200

    customer name added

--- src/common/components/ShopArea.jsx
+++ src/common/components/ShopArea.jsx
@@ -1,15 +1,33 @@
 import { useDispatch } from 'react-redux';
 import { removeItem, addItem } from '../../features/cart/cartSlice';
+import { useSelector } from 'react-redux';
 
 export const ShopArea = () => {
+	const { customerName } = useSelector((state) => state.cart);
 	const dispatch = useDispatch();
 
 	return (
 		<div className="page_shopArea">
-			<h2>Shop Area</h2>
+			{customerName !== '' ? (
+				<h2>{customerName}'s Shop</h2>
+			) : (
+				<h2>General Shop</h2>
+			)}
 			<button onClick={() => dispatch(removeItem())}>-</button>
-			<button onClick={() => dispatch({type: 'cart/addItem', payload: 'milk'})}>Milk</button>
-			<button onClick={() => dispatch({type: 'cart/addItem', payload: 'sugar'})}>Sugar</button>
+			<button
+				onClick={() =>
+					dispatch({ type: 'cart/addItem', payload: 'milk' })
+				}
+			>
+				Milk
+			</button>
+			<button
+				onClick={() =>
+					dispatch({ type: 'cart/addItem', payload: 'sugar' })
+				}
+			>
+				Sugar
+			</button>
 		</div>
 	);
 };
--- src/common/components/ShoppingCart.jsx
+++ src/common/components/ShoppingCart.jsx
@@ -1,11 +1,14 @@
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 
 export const ShoppingCart = () => {
-	const items = useSelector((state) => state.cart.items);
+	const { items, customerName } = useSelector((state) => state.cart);
+	const dispatch = useDispatch();
 
 	return (
 		<div className="page_shoppingCart">
 			<h2>Shopping Cart</h2>
+			<button onClick={() => dispatch({ type: 'cart/addCustomerName', payload: 'Henri' })}>Add Customer Name</button> {customerName}
 			<div>items in cart: {items.length}</div>
 			<ul>
 				{items.map((item, index) => {
