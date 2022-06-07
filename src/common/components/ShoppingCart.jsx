8ffe7a759ea3e5d65fc1b94b2d8238bc73557509
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-07 14:53:46 +0200

    adding products

--- src/common/components/ShoppingCart.jsx
+++ src/common/components/ShoppingCart.jsx
@@ -1,13 +1,17 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export const ShoppingCart = () => {
-	const count = useSelector((state) => state.counter.value);
-	const dispatch = useDispatch();
+	const items = useSelector((state) => state.cart.items);
 
 	return (
 		<div className="page_shoppingCart">
 			<h2>Shopping Cart</h2>
-			<div>Count: {count}</div>
+			<div>items in cart: {items.length}</div>
+			<ul>
+				{items.map((item, index) => {
+					return <li key={index}>{item}</li>;
+				})}
+			</ul>
 		</div>
 	);
 };
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,24 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-	value: 0,
+	items: [],
 };
 
-export const counterSlice = createSlice({
-	name: 'counter',
+export const cartSlice = createSlice({
+	name: 'cart',
 	initialState,
 	reducers: {
 		increment: (state) => {
-			state.value += 1;
+			const _items = state.items;
+			_items.push('nnn');
+			state.items = _items;
+			console.log(state.items);
 		},
 		decrement: (state) => {
-			state.value -= 1;
-		},
-		incrementByAmount: (state, action) => {
-			state.value += action.payload;
-		},
+			state.items.pop();
+		}
 	},
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
-export default counterSlice.reducer;
+export const { increment, decrement} = cartSlice.actions;
+export default cartSlice.reducer;
